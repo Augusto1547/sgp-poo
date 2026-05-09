ffc6ddb4a0841899b74926bc61fe3256740f1f7e
Agusto César <negocios.augusto@gmail.com>
2026-05-09 09:41:23 -0300

    Criação do arquivo gitignore

---
+++ src/entidades/Usuario.java
@@ -0,0 +1,19 @@
+package entidades;
+
+import enums.StatusUsuario;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private StatusUsuario status;
+
+    public Usuario(){
+
+
+    }
+    
+}
